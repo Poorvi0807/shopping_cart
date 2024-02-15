212596a4cbc161f4632a67cb14774aa7989ae8b6
Poorvi Singh Thakur <poorvisinghthakur27@gmail.com>
2024-02-15 23:03:50 +0530

    data

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,18 +4,20 @@ import { items } from './Data'
 const Product = () => {
   return (
     <>
-    <div className='container'>
+    <div className='container my-5 text-center'>
     <div className='row'>
         {items.map((product)=>(
           
             <>
-            <div className='col-lg-4'>
+            <div className='col-lg-4 my-3'>
             <div className="card" style={{width: '18rem'}}>
-  <img src="..." className="card-img-top" alt="..." />
+  <img src={product.imgSrc} className="card-img-top" alt="..." />
   <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
+    <h5 className="card-title">{product.title}</h5>
+    <p className="card-text">{product.description}</p>
+    <button className='btn btn-primary'>{product.price}</button>
+    <button className='btn btn-primary mx-3'>Add To Cart</button>
+    
     </div>
   </div>
 </div>
