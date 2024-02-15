272406d37d071152401f0d93eabe302f7cb3bd93
Poorvi Singh Thakur <poorvisinghthakur27@gmail.com>
2024-02-15 23:11:01 +0530

    dataAdded

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Product from './components/Product'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import ProductDetail from './components/ProductDetail'
 const App = () => {
   return (
-  
     <>
+    <Router>
     <Navbar />
-    <Product />
+    <Routes>
+      <Route path='/' element={<Product />}></Route>
+      <Route path='/' element={<ProductDetail />}></Route>
+    </Routes>
+    
+    </Router>
     </>
-  
   )
 }
 
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -15,7 +15,7 @@ const Product = () => {
   <div className="card-body">
     <h5 className="card-title">{product.title}</h5>
     <p className="card-text">{product.description}</p>
-    <button className='btn btn-primary'>{product.price}</button>
+    <button className='btn btn-primary'>{product.price} ₹</button>
     <button className='btn btn-primary mx-3'>Add To Cart</button>
     
     </div>
