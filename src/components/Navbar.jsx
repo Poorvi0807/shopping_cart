8f36c9b5f42668bbb3ae4f09b892cc676fd7692b
Poorvi Singh Thakur <poorvisinghthakur27@gmail.com>
2024-02-17 11:51:51 +0530

    SearchHAlf

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
-import {Link} from "react-router-dom";
+import React, { useState } from 'react'
+import {Link, useNavigate} from "react-router-dom";
 import { items } from './Data';
 const Navbar = ({setData}) => {
+   const navigate=useNavigate();
+  const [searchTerm,setSearchTerm] = useState("");
 
   const filterByCategory = (category)=>{
     const element = items.filter((product)=> product.category===category);
@@ -10,18 +12,24 @@ const Navbar = ({setData}) => {
   }
 
   const filterByPrice = (price)=>{
-    const element = items.filter((product)=> product.price === price);
+    const element = items.filter((product)=> product.price >= price);
     console.log(element);
     setData(element);
+  }
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    navigate(`/search/${searchTerm}`);
+    
   }
   return (
     <header className='sticky-top'>
     <div className='nav-bar'>
  
     <Link to={'/'} className='brand'>E-cart</Link>
-    <div className='search-bar'>
-        <input type='text' placeholder='Search Products'/>
-    </div>
+    <form onSubmit={handleSubmit} className='search-bar'>
+        <input type='text' value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)} placeholder='Search Products'/>
+    </form>
      <Link to={'/cart'} className='cart'>Cart</Link>
     </div>
     <div className='nav-bar-wrapper'>
@@ -30,10 +38,10 @@ const Navbar = ({setData}) => {
     <div className='items' onClick={()=>filterByCategory('mobiles')}>Mobiles</div>
     <div className='items' onClick={()=>filterByCategory('laptops')}>Laptops</div>
     <div className='items'  onClick={()=>filterByCategory('tablets')}>Tablets</div>
-    <div className='items'>{">="}29999</div>
-    <div className='items'>{">="}49999</div>
-    <div className='items'>{">="}69999</div>
-    <div className='items'>{">="}89999</div>
+    <div className='items'  onClick={()=> filterByPrice(29999)}>{">="}29999</div>
+    <div className='items' onClick={()=> filterByPrice(49999)}>{">="}49999</div>
+    <div className='items' onClick={()=> filterByPrice(69999)}>{">="}69999</div>
+    <div className='items'  onClick={()=> filterByPrice(89999)}>{">="}89999</div>
 
     </div>
     </header>
